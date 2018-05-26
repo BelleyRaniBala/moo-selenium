5358cb19828d294d0ba86943484976763933b438
Belley Rani Bala <balabelley@gmail.com>
2018-05-26 10:26:03 +0100

    initial commit

---
+++ src/main/java/com/example/moo/MooApplication.java
@@ -0,0 +1,12 @@
+package com.example.moo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MooApplication.class, args);
+	}
+}
