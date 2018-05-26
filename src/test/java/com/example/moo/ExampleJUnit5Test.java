543c4e8bbe4ec8eafaab4a4e454b15f28ef2686f
Belley Rani Bala <balabelley@gmail.com>
2018-05-26 10:40:05 +0100

    adding selenium and chromedriver

--- src/test/java/com/example/moo/ExampleJUnit5Test.java
+++ src/test/java/com/example/moo/ExampleJUnit5Test.java
@@ -1,10 +1,14 @@
 package com.example.moo;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
 
- class ExampleJUnit5Test {
+class ExampleJUnit5Test {
     @Test
     void justATestOfTests(){
-        System.out.println("test");
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com");
     }
 }
